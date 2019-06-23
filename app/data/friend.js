15bbe5d5864c198aad5a2091af9fc985d07e75c9
Bethany <bethany.grogg@gmail.com>
2019-06-22 21:06:40 -0400

    working on logic for modal

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,5 +1,35 @@
-var friendsArray = [
-    
+var friendsArray = [ {
+    "name": "Beth",
+    "photo": "photo",
+    "scores": [
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1"
+    ]
+    },
+    {
+    "name": "BethanyG",
+    "photo": "photo",
+    "scores": [
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2"
+    ]
+    }  
 ];
 
 module.exports = friendsArray;
--- app/routing/apiroutes.js
+++ app/routing/apiroutes.js
@@ -15,12 +15,46 @@ app.get("/api/friends", function(req, res) {
     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
    // newTables.routeName = newTables.name.replace(/\s+/g, "").toLowerCase();
   
- //   console.log(newFriends);
-    
-  
     tableData.push(req.body);
   
-//    res.json(tableData);
- 
+    console.log(tableData);
+    console.log(req.body.scores[0]);
+
+    var newArray = [];
+
+    for (i = 0; i < 10; i++) {
+
+      var newScore = parseInt(tableData[0].scores[i]);
+      
+      newArray.push(newScore);
+       
+    }
+    
+    var j = tableData.length - 1;
+   
+    var friendsArray = [];
+   
+    for (i = 0; i < (tableData.length - 1); i++) {
+      var newAnswer = 0;
+      for (k = 0; k < 10; k++) {
+         
+         console.log("currentUser: " + tableData[j].scores[k]);
+         console.log("otherFriends: " + tableData[i].scores[k]);
+        var answer = Math.abs(tableData[j].scores[k] - tableData[i].scores[k]);
+        newAnswer += answer;
+        
+      }
+      console.log("newAnswer: " + newAnswer);
+      friendsArray.push(newAnswer);
+      console.log('friendsArray: ' + friendsArray);
+    }
+
+    console.log("array sort: " + friendsArray.sort(function(a, b){return a - b}));
+    
+
+    //    res.json(tableData);
+    res.redirect(req.originalUrl);
 });
+
+
 };  
