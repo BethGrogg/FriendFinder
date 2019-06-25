11829ab110a977ad5933fabfcf07f74c787d19dc
Bethany <bethany.grogg@gmail.com>
2019-06-24 20:37:56 -0400

    modal working with image showing

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,6 +1,6 @@
 var friendsArray = [ {
     "name": "Beth",
-    "photo": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn1.medicalnewstoday.com%2Fcontent%2Fimages%2Farticles%2F322%2F322868%2Fgolden-retriever-puppy.jpg&imgrefurl=https%3A%2F%2Fwww.medicalnewstoday.com%2Farticles%2F322868.php&docid=zy7o1vNTZaWwDM&tbnid=wsZAvWshu1RTDM%3A&vet=10ahUKEwjrttK54YLjAhWeAp0JHd7WDJEQMwiBASgEMAQ..i&w=1100&h=734&bih=578&biw=1280&q=dogs&ved=0ahUKEwjrttK54YLjAhWeAp0JHd7WDJEQMwiBASgEMAQ&iact=mrc&uact=8",
+    "photo": "https://www.nationalgeographic.com/content/dam/animals/thumbs/rights-exempt/mammals/d/domestic-dog_thumb.jpg",
     "scores": [
     "1",
     "1",
@@ -16,7 +16,7 @@ var friendsArray = [ {
     },
     {
     "name": "BethanyG",
-    "photo": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.nationalgeographic.com%2Fcontent%2Fdam%2Fanimals%2Fthumbs%2Frights-exempt%2Fmammals%2Fd%2Fdomestic-dog_thumb.jpg&imgrefurl=https%3A%2F%2Fwww.nationalgeographic.com%2Fanimals%2Fmammals%2Fd%2Fdomestic-dog%2F&docid=vLwAsZFk0qz-xM&tbnid=iDXtudaUj_c97M%3A&vet=10ahUKEwjrttK54YLjAhWeAp0JHd7WDJEQMwiAASgDMAM..i&w=2048&h=2048&bih=578&biw=1280&q=dogs&ved=0ahUKEwjrttK54YLjAhWeAp0JHd7WDJEQMwiAASgDMAM&iact=mrc&uact=8",
+    "photo": "",
     "scores": [
     "2",
     "2",
